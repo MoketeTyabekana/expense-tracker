f9d2f285faadd04f71d0399207b5ccad51e69d4b
Mokete <emtyabekana@gmail.com>
2024-11-02 10:44:34 +0200

    Imported our html elements into our js file for accessibilty and manupulation of  their inner content.

---
+++ script.js
@@ -0,0 +1,7 @@
+// Getting our elements from our html file though their ID
+
+const itemName =document.getElementById('expense-name');
+const itemPrice =document.getElementById('expense-amount');
+const addBtn =document.getElementById('add-expense');
+const totalAmount =document.getElementById('total-amount');
+const listItems =document.getElementById('expense-list');
